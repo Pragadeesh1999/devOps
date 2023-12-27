dd2043328c3e3889e4a1008377b86c4d31ee2d1e
PragadeeshMadurai <pragapraga2509@gmail.com>
2023-12-27 16:40:36 +0530

    first commit'

---
+++ src/main/java/com/devOps/controller/DevOpsController.java
@@ -0,0 +1,13 @@
+package com.devOps.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DevOpsController {
+
+	@GetMapping(value="/get")
+	public String get() {
+		return "hello";
+	}
+}
